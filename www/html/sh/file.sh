d6cff1d9e9ba7620f105e26665bcdd71d3e40d14
Juan Romero <romero126@gmail.com>
2020-01-04 22:21:00 -0800

    Update file.sh to support git autoupdate.

--- www/html/sh/file.sh
+++ www/html/sh/file.sh
@@ -4,3 +4,11 @@ echo "Content-type: text/html"
 echo ""
 echo "<html><head><title>Test</title></head><body>"
 echo "Today is $DATE <br>"
+
+echo "<pre>"
+ls
+
+git branch
+
+git pull
+echo "</pre>" 
